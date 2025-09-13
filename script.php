788731b2f03acec69252b415adf301a8b3798690
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-09-13 17:25:29 +0200

    2.3.1
    Use ConseilGouz library

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* CG Parallax Component  - Joomla 4.x/5.x/6.x Component 
+* CG Parallax Component  - Joomla 4.x/5.x/6.x Component
 * Package			: CG Parallax
 * copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 * license    		: https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
@@ -9,6 +9,7 @@
 // No direct access to this file
 defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
+// use Joomla\CMS\Installer\Installer;
 use Joomla\CMS\Language\Text;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Filesystem\File;
@@ -16,149 +17,154 @@ use Joomla\Filesystem\Folder;
 
 class com_cgparallaxInstallerScript
 {
-	private $min_joomla_version      = '4.0';
-	private $min_php_version         = '8.0';
-	private $name                    = 'CG Parallax';
-	private $exttype                 = 'component';
-	private $extname                 = 'cgisotope';
-	private $previous_version        = '';
-	private $dir           = null;
-	private $lang = null;
-	private $installerName = 'cgparallaxinstaller';
-	public function __construct()
-	{
-		$this->dir = __DIR__;
-		$this->lang = Factory::getLanguage();
-		$this->lang->load($this->extname);
-	}
-    function preflight($type, $parent)
+    private $min_joomla_version      = '4.0';
+    private $min_php_version         = '8.0';
+    private $name                    = 'CG Parallax';
+    private $exttype                 = 'component';
+    private $extname                 = 'cgparallax';
+    private $previous_version        = '';
+    private $newlib_version			 = '';
+    private $dir  = null;
+    private $lang = null;
+    private $installerName = 'cgparallaxinstaller';
+    public function __construct()
+    {
+        $this->dir = __DIR__;
+        $this->lang = Factory::getApplication()->getLanguage();
+        $this->lang->load($this->extname);
+    }
+    public function preflight($type, $parent)
     {
 
-		if ( ! $this->passMinimumJoomlaVersion())
-		{
-			$this->uninstallInstaller();
+        if (! $this->passMinimumJoomlaVersion()) {
+            $this->uninstallInstaller();
 
-			return false;
-		}
+            return false;
+        }
 
-		if ( ! $this->passMinimumPHPVersion())
-		{
-			$this->uninstallInstaller();
+        if (! $this->passMinimumPHPVersion()) {
+            $this->uninstallInstaller();
 
-			return false;
-		}
-		// To prevent installer from running twice if installing multiple extensions
-		if ( ! file_exists($this->dir . '/' . $this->installerName . '.xml'))
-		{
-			return true;
-		}
-		$xml = simplexml_load_file(JPATH_ADMIN . '/components/com_'.$this->extname.'/'.$this->extname.'.xml');
-		$this->previous_version = $xml->version;
-		
+            return false;
+        }
+        // To prevent installer from running twice if installing multiple extensions
+        if (! file_exists($this->dir . '/' . $this->installerName . '.xml')) {
+            return true;
+        }
+        $xml = simplexml_load_file(JPATH_ADMIN . '/components/com_'.$this->extname.'/'.$this->extname.'.xml');
+        $this->previous_version = $xml->version;
+
     }
-    
-    function install($parent)
+    public function postflight($type, $parent)
     {
+        if (($type == 'install') || ($type == 'update')) { // remove obsolete dir/files
+            $this->postinstall_cleanup();
+        }
+        if (!$this->checkLibrary('conseilgouz')) { // need library installation
+            $ret = $this->installPackage('lib_conseilgouz');
+            if ($ret) {
+                Factory::getApplication()->enqueueMessage('ConseilGouz Library ' . $this->newlib_version . ' installed', 'notice');
+
+            }
+        }
+        // Uninstall this installer
+        $this->uninstallInstaller();
+        return true;
     }
-    
-    function uninstall($parent)
+    private function checkLibrary($library)
     {
+        $xml = simplexml_load_file($this->dir.'/lib_conseilgouz/conseilgouz.xml');
+        $this->newlib_version = $xml->version;
+
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $conditions = array(
+             $db->qn('type') . ' = ' . $db->q('library'),
+             $db->qn('element') . ' = ' . $db->quote($library)
+            );
+        $query = $db->getQuery(true)
+                ->select('manifest_cache')
+                ->from($db->quoteName('#__extensions'))
+                ->where($conditions);
+        $db->setQuery($query);
+        $manif = $db->loadObject();
+        if ($manif) {
+            $manifest = json_decode($manif->manifest_cache);
+            if ($manifest->version <= $this->newlib_version) { // compare versions
+                return true; // library ok
+            }
+        }
+        return false; // need library
     }
-    
-    function update($parent)
+    private function installPackage($package)
     {
+        $tmpInstaller = new Joomla\CMS\Installer\Installer();
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $tmpInstaller->setDatabase($db);
+        $installed = $tmpInstaller->install($this->dir . '/' . $package);
+        return $installed;
     }
-    
-    function postflight($type, $parent)
+
+    private function postinstall_cleanup()
     {
-		if (($type=='install') || ($type == 'update')) { // remove obsolete dir/files
-			$this->postinstall_cleanup();
-		}
-		// Uninstall this installer
-		$this->uninstallInstaller();
+        // remove obsolete update sites
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__update_sites')
+            ->where($db->quoteName('location') . ' like "%432473037d.url-de-test.ws/%"');
+        $db->setQuery($query);
+        $db->execute();
+        // Simple Isotope is now on Github
+        $query = $db->getQuery(true)
+            ->delete('#__update_sites')
+            ->where($db->quoteName('location') . ' like "%conseilgouz.com/updates/com_cgparallax%"');
+        $db->setQuery($query);
+        $db->execute();
 
-		return true;
-
-
-//         JFactory::getApplication()->enqueueMessage($message);       
     }
-	private function postinstall_cleanup() {
-		
-		/* $obsoleteFiles = [
-			JPATH_ADMINISTRATOR."/components/com_cgisotope/updates.txt"
-		];
-		foreach ($obsoleteFiles as $file) {
-			if (@is_file($file)) {
-				File::delete($file);
-			}
-		}
-		*/
-		// remove obsolete update sites
-		$db = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->getQuery(true)
-			->delete('#__update_sites')
-			->where($db->quoteName('location') . ' like "%432473037d.url-de-test.ws/%"');
-		$db->setQuery($query);
-		$db->execute();
-		// Simple Isotope is now on Github
-		$query = $db->getQuery(true)
-			->delete('#__update_sites')
-			->where($db->quoteName('location') . ' like "%conseilgouz.com/updates/com_cgparallax%"');
-		$db->setQuery($query);
-		$db->execute();
-		
-	}	
-	// Check if Joomla version passes minimum requirement
-	private function passMinimumJoomlaVersion()
-	{
-		if (version_compare(JVERSION, $this->min_joomla_version, '<'))
-		{
-			Factory::getApplication()->enqueueMessage(
-				'Incompatible Joomla version : found <strong>' . JVERSION . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
-				'error'
-			);
+    // Check if Joomla version passes minimum requirement
+    private function passMinimumJoomlaVersion()
+    {
+        if (version_compare(JVERSION, $this->min_joomla_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible Joomla version : found <strong>' . JVERSION . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
+                'error'
+            );
+            return false;
+        }
+        return true;
+    }
+    // Check if PHP version passes minimum requirement
+    private function passMinimumPHPVersion()
+    {
+        if (version_compare(PHP_VERSION, $this->min_php_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
+                'error'
+            );
+            return false;
+        }
+        return true;
+    }
 
-			return false;
-		}
-
-		return true;
-	}
-
-	// Check if PHP version passes minimum requirement
-	private function passMinimumPHPVersion()
-	{
-
-		if (version_compare(PHP_VERSION, $this->min_php_version, '<'))
-		{
-			Factory::getApplication()->enqueueMessage(
-					'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
-				'error'
-			);
-			return false;
-		}
-
-		return true;
-	}
-	
-	private function uninstallInstaller()
-	{
-		if ( ! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
-			return;
-		}
-		$this->delete([
-			JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
-			JPATH_PLUGINS . '/system/' . $this->installerName,
-		]);
-		$db = Factory::getContainer()->get(DatabaseInterface::class);
-		$query = $db->getQuery(true)
-			->delete('#__extensions')
-			->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
-			->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
-			->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
-		$db->setQuery($query);
-		$db->execute();
-		Factory::getCache()->clean('_system');
-	}
+    private function uninstallInstaller()
+    {
+        if (! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
+            return;
+        }
+        $this->delete([
+            JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
+            JPATH_PLUGINS . '/system/' . $this->installerName,
+        ]);
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__extensions')
+            ->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
+            ->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+        $db->setQuery($query);
+        $db->execute();
+        Factory::getApplication()->getCache()->clean('_system');
+    }
     public function delete($files = [])
     {
         foreach ($files as $file) {
