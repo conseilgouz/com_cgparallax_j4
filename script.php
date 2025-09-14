a00eae63a958a494c93768199b901d828d5efb4c
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-09-14 17:42:36 +0200

    2.3.2
    ConseilGouz Library 1.0.3

--- script.php
+++ script.php
@@ -96,7 +96,7 @@ class com_cgparallaxInstallerScript
         $manif = $db->loadObject();
         if ($manif) {
             $manifest = json_decode($manif->manifest_cache);
-            if ($manifest->version <= $this->newlib_version) { // compare versions
+            if ($manifest->version >= $this->newlib_version) { // compare versions
                 return true; // library ok
             }
         }
